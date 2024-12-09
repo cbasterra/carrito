745f8b8ac61e242a08910858611031a3b6a9ceb2
cbasterra <cirobasterra0@gmail.com>
2024-12-09 13:14:15 -0300

    login component + initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
