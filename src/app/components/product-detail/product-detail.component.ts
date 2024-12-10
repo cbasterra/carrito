53494d7921760304611a49a3a9fc09fcb4865a0d
cbasterra <cirobasterra0@gmail.com>
2024-12-09 23:41:55 -0300

    producto details

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosServiceService } from 'src/app/services/productos-service.service';
+import { Producto } from 'src/interfaces/productos';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  constructor(private productosService: ProductosServiceService, private route: ActivatedRoute) {}
+
+  productos: Producto[] = [];
+  producto: Producto = {
+    id: 0,
+    title: '',
+    price: 0,
+    imagen: '',
+    categoria: '',
+  };
+  idproducto: string = '';
+
+  ngOnInit(): void {
+    this.productosService.getProductos().subscribe((productos) => {
+      this.producto = productos.find(producto => producto.id == this.route.snapshot.params['idproducto']) || this.producto;
+    });
+  }
 
 }
